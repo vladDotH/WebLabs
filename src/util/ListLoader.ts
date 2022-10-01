f5055c1f73d17db490111ae633ce424797a629a5
Vladislav <vladbolkunovv@gmail.com>
2022-10-01 14:58:31 +0300

    fixes

--- src/util/ListLoader.ts
+++ src/util/ListLoader.ts
@@ -1,6 +1,7 @@
 import { config, RequestType } from "@/../api";
 import axios from "axios";
 
+// Контроллер списка книг
 export class ListLoader {
   readonly books: number[] = [];
 
@@ -12,6 +13,7 @@ export class ListLoader {
     return new URL(config.endpoints.book, config.server).toString();
   }
 
+  // Загрузка списка с заданным фильтром
   async fetch(req: RequestType) {
     const url = this.listUrl();
     url.searchParams.append(config.reqTypeName, req.toString());
@@ -19,11 +21,13 @@ export class ListLoader {
     this.books.splice(0, this.books.length, ...res.data);
   }
 
+  // Добавление книги
   async add(book: FormData) {
     const res = await axios.post<number>(this.addUrl, book);
     this.books.unshift(res.data);
   }
 
+  // Удаление книги
   remove(id?: number) {
     const index = id ? this.books.indexOf(id) : -1;
     if (index !== -1) this.books.splice(index, 1);
